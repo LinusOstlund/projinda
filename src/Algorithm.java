ad58dc1115c4409fc76b67927bf788198f0bbd83
Linus Östlund <linusostlund@linuss-air.lan>
2022-05-12 13:47:06 +0200

    add src directory

---
+++ src/Algorithm.java
@@ -0,0 +1,10 @@
+public class Algorithm {
+  public static void main(String[] args){
+  int sum = 0;
+	int foo = 5;
+  for(int i = 0; i < 10; i++){
+      sum++;
+    }
+  System.out.println(sum);
+  }
+}
